6fff9b30ab1e7089a5885057627405d189a5a4d1
e-fa696 <e.f696@outlook.fr>
2025-01-07 09:23:00 +0100

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,37 @@
-from cryptography.fernet import Fernet
 from flask import Flask, render_template, request, jsonify
-import sqlite3
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
 
-# Route d'accueil
-@app.route('/')
-def hello_world():
-    return render_template('hello.html')
-
-# Fonction générant une clé (uniquement une fois lors du démarrage)
+# Clé de cryptage
 key = Fernet.generate_key()
 f = Fernet(key)
+
+# Page d'accueil avec des catégories cliquables
+@app.route('/')
+def accueil():
+    return render_template('index.html')
+
+# Routes pour chaque exercice
+@app.route('/exercice1')
+def exercice1():
+    return render_template('exercice1.html')
+
+@app.route('/exercice2')
+def exercice2():
+    return render_template('exercice2.html')
+
+@app.route('/exercice3')
+def exercice3():
+    return render_template('exercice3.html')
+
+@app.route('/exercice4')
+def exercice4():
+    return render_template('exercice4.html')
+
+@app.route('/exercice5')
+def exercice5():
+    return render_template('exercice5.html')
 
 # Route pour crypter une valeur
 @app.route('/encrypt/<string:valeur>')
@@ -37,11 +57,11 @@ def set_key():
     if not user_key:
         return jsonify({"error": "No key provided"}), 400  # Si aucune clé n'est fournie
     try:
+        global f  # Modifier la variable globale f
         f = Fernet(user_key.encode())  # Créer un nouvel objet Fernet avec la clé de l'utilisateur
         return jsonify({"message": "Key set successfully"}), 200  # Réponse indiquant que la clé a été définie
     except Exception as e:
         return jsonify({"error": f"Invalid key: {str(e)}"}), 400  # Si la clé est invalide
 
-# Lancer l'application Flask
 if __name__ == "__main__":
     app.run(debug=True)
